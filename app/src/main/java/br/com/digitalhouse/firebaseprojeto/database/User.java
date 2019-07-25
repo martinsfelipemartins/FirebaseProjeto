56f72433646c767f8f4ae0a1fd949bba7eee58be
martinsfelipemartins <martins.felipe1@hotmail.com>
2019-07-24 22:16:19 -0300

    Initial commit

---
+++ app/src/main/java/br/com/digitalhouse/firebaseprojeto/database/User.java
@@ -0,0 +1,20 @@
+package br.com.digitalhouse.firebaseprojeto.database;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class User {
+
+    public String name;
+    public String email;
+
+    // Default constructor required for calls to
+    // DataSnapshot.getValue(User.class)
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
